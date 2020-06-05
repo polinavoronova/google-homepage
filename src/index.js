11a208a2e3fc0cb5f71ace3c10ac8ef6e25c7fea
polina.voronova <polonium.pv@gmail.com>
2020-06-05 11:59:21 +0300

    feat: implement show/hide functionallity for google settings

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 const googleApps = document.getElementById('google-apps');
 const googleAppsWindow = document.getElementById('google-apps-window');
 
-googleApps.addEventListener('mouseup', showHide);
+const googleSettings = document.getElementById('google-settings');
+const googleSettingsWindow = document.getElementById('settings-window');
 
-function showHide() {
+googleApps.addEventListener('mouseup', function(){
     if(googleAppsWindow.style.visibility === 'visible') {
         googleAppsWindow.style.visibility = 'hidden';
     } else {
         googleAppsWindow.style.visibility = 'visible';
     }
-}
+});
+
+googleSettings.addEventListener('mouseup', function() {
+    if(googleSettingsWindow.style.visibility === 'visible') {
+        googleSettingsWindow.style.visibility = 'hidden';
+    } else {
+        googleSettingsWindow.style.visibility = 'visible';
+        document.getElementsByClassName('footer-menu__link')[6].style.color = '#5f6368';
+        document.getElementsByClassName('footer-menu__link')[6].style.textDecoration = 'none';
+    }
+});
